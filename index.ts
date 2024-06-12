52754307372f03529d01785568c68d4a2e740eff
Aashra <saleemlatif17@gmail.com>
2024-06-11 23:43:57 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let items =[];
+let condition = true;
+while(condition)
+{let shop = await inquirer.prompt(
+    [
+        {
+            name:'firstQuestion',
+            type:'input',
+            message:'What would you like to add in your cart?'
+        },{
+            name:'secondQuestion',
+            type:'confirm',
+            message:'Would you like to add more in your cart?',
+            default:false
+        }
+    ]
+);
+items.push(shop.firstQuestion);
+console.log(items);         
+condition = shop.secondQuestion;
+};
---
+++ main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let list = [];
+let condition = true;
+while(condition){
+let Tasks = await inquirer.prompt(
+    [
+        {
+            name: "addTask",
+            type: "input",
+            message: "What do you want to add in your list?" 
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Are you sure you want to add more in your list?",
+            default: "false"
+        }
+    ]
+)
+list.push(Tasks.addTask);
+condition = Tasks.addMore;
+console.log(list);
+};
